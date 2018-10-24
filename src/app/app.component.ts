f1f83b6fe9486e4adbcf5430e2fdef815a49f8b3
Raphael Katana <roduor41@gmail.com>
2018-10-24 17:20:13 +0300

    add changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import{Goal}from './goal'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'practise';
+  title = 'Angular Catch-up';
+  public h2='My Goals'
+  
+  
+  constructor(){
+  }
 }
+
